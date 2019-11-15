78b1afbc0aeb3db85a0bbde6c26616aaea36c7f7
Yann Lastapis <yann.lastapis@innov-atm.com>
2019-11-15 21:42:18 +0100

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback, useState } from 'react';
 import './App.scss';
-import { Container, FormControl, TextField } from '@material-ui/core';
+import { Container, FormControl, FormControlLabel, FormGroup, Switch, TextField } from '@material-ui/core';
 import DateFnsUtils from '@date-io/moment';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { Moment } from 'moment';
 import { getMoment } from './date';
-import { WeatherFetcher } from './WeatherFetcher';
+import { Weather } from './Weather';
 
 const App: React.FC = () => {
     const [city, setCity] = useState<string>('');
     const [date, setDate] = useState<Moment | null>(getMoment());
+    const [visible, setVisible] = useState(true);
     const handleKeyUp = useCallback((e) => {
         if (e.key === 'Enter') {
             setCity(e.target.value);
@@ -37,11 +38,25 @@ const App: React.FC = () => {
                             id="city"
                         />
                     </FormControl>
+                    <br/>
+                    <FormControl>
+                        <FormGroup aria-label="position" row>
+                            <FormControlLabel
+                                control={<Switch
+                                    checked={visible}
+                                    onChange={() => setVisible(oldState => !oldState)}
+                                    value="checkedB"
+                                    color="primary"
+                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                />}
+                                label="Visible"
+                            />
+                        </FormGroup>
+                    </FormControl>
                 </div>
                 <div className="right">
-                    <WeatherFetcher city={city} date={date} />
+                    {visible && city && (<Weather city={city} date={date} />)}
                 </div>
-
             </Container>
         </div>
     );
--- src/Weather.tsx
+++ src/Weather.tsx
@@ -8,96 +8,143 @@ import { CircularProgress } from '@material-ui/core';
 import { Moment } from 'moment';
 
 interface WeatherProps {
-    data: OpenWeatherForecastApiResponse | null;
-    loading: boolean;
-    fetchErrors?: string | null;
+    city: string;
     date: Moment | null;
 }
-export const Weather = ({
-    data,
-    loading,
-    fetchErrors,
-    date,
-}: WeatherProps) => {
-    if (fetchErrors) {
-        return <span>Errors occurred during request</span>
+interface WeatherState {
+    data: null | OpenWeatherForecastApiResponse;
+    loading: boolean;
+    fetchError: null | any;
+}
+export class Weather extends React.Component<WeatherProps, WeatherState> {
+    constructor(props: WeatherProps) {
+        super(props);
+        this.state = {
+            data: null,
+            loading: false,
+            fetchError: null,
+        };
     }
-    const { todayIcon, cityName, current, list, err } = processData(data, date);
-    return (
-        <div className="rw-box">
-            <div className="rw-main">
-                {loading ? (
-                    <CircularProgress disableShrink />
-                ) : current && (
-                    <>
-                        <div className="rw-box-left">
-                            <h2>{cityName}</h2>
-                            <div className="rw-today">
-                                <div className="date">
-                                    {getMoment(current.dt).format('LLL')}
+
+    componentDidMount(): void {
+        if (this.props.city) {
+            this.handleFetch();
+        }
+    }
+
+    componentDidUpdate(prevProps: Readonly<WeatherProps>, prevState: Readonly<WeatherState>, snapshot?: any): void {
+        if (this.props.city && prevProps.city !== this.props.city) {
+            this.handleFetch();
+        }
+    }
+
+    handleFetch() {
+        this.setState({
+            loading: true,
+            fetchError: null,
+        });
+
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=abdd4534fe73e0245449d2a7dee7ca3a&q=${this.props.city}&lang=fr&units=metric`)
+            .then(response => response.json())
+            .then((data : OpenWeatherForecastApiResponse)=> this.setState({
+                data
+            }))
+            .catch(e => this.setState({
+                fetchError: e
+            }))
+            .finally(() => this.setState({
+                loading: false
+            }));
+    }
+
+    render() {
+        const {
+            data,
+            loading,
+            fetchError,
+        } = this.state;
+        const { date } = this.props;
+
+        if (fetchError) {
+            return <span>Errors occurred during request</span>
+        }
+        const { todayIcon, cityName, current, list, err } = processData(data, date);
+        return (
+            <div className="rw-box">
+                <div className="rw-main">
+                    {loading ? (
+                        <CircularProgress disableShrink />
+                    ) : current && (
+                        <>
+                            <div className="rw-box-left">
+                                <h2>{cityName}</h2>
+                                <div className="rw-today">
+                                    <div className="date">
+                                        {getMoment(current.dt).format('LLL')}
+                                    </div>
+                                    <div className="hr"/>
+                                    <div className="current">
+                                        {current.main.temp}°C
+                                        <div className="range">
+                                            <span>{current.main.temp_min}°C</span>
+                                            <span>/</span>
+                                            <span>{current.main.temp_max}°C</span>
+                                        </div>
+                                    </div>
+                                    <div className="hr"/>
+                                    <div className="info">
+                                        <div>
+                                            Vent: <b>{current.wind.speed.toFixed(0)}</b> km/h
+                                        </div>
+                                        <div>
+                                            Humidité: <b>{current.main.humidity}</b> %
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="hr"/>
-                                <div className="current">
-                                    {current.main.temp}°C
-                                    <div className="range">
-                                        <span>{current.main.temp_min}°C</span>
+                            </div>
+                            <div className="rw-box-right">
+                                {todayIcon && <WeatherIcon icon={todayIcon}/>}
+                            </div>
+                        </>
+                    )}
+                </div>
+                <div
+                    className={classNames("rw-box-days", {
+                        empty: !list || list.length === 0
+                    })}
+                >
+                    {!loading && list && list.length > 0 ? (
+                        list.map((day, i) => (
+                            <React.Fragment key={i}>
+                                <div key={`day-${i}`} className="rw-day">
+                                    <div className="rw-date">
+                                        {getMoment(day.dt).format('DD/MM/YYYY')}
+                                    </div>
+                                    <WeatherIcon icon={day.weather[0].icon}/>
+                                    <div className="rw-desc">{day.weather[0].description}</div>
+                                    <div className="rw-range">
+                                        <span>{day.main.temp_min}°C</span>
                                         <span>/</span>
-                                        <span>{current.main.temp_max}°C</span>
-                                    </div>
-                                </div>
-                                <div className="hr"/>
-                                <div className="info">
-                                    <div>
-                                        Vent: <b>{current.wind.speed.toFixed(0)}</b> km/h
+                                        <span>{day.main.temp_max}</span>°C
                                     </div>
                                     <div>
-                                        Humidité: <b>{current.main.humidity}</b> %
+                                        Vent: <b>{day.wind.speed.toFixed(0)}</b> km/h
+                                    </div>
+                                    <div>
+                                        Humidité: <b>{day.main.humidity}</b> %
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="rw-box-right">
-                            {todayIcon && <WeatherIcon icon={todayIcon}/>}
-                        </div>
-                    </>
-                )}
-            </div>
-            <div
-                className={classNames("rw-box-days", {
-                    empty: !list || list.length === 0
-                })}
-            >
-                {!loading && list && list.length > 0 ? (
-                    list.map((day, i) => (
-                        <React.Fragment key={i}>
-                            <div key={`day-${i}`} className="rw-day">
-                                <div className="rw-date">
-                                    {getMoment(day.dt).format('DD/MM/YYYY')}
-                                </div>
-                                <WeatherIcon icon={day.weather[0].icon}/>
-                                <div className="rw-desc">{day.weather[0].description}</div>
-                                <div className="rw-range">
-                                    <span>{day.main.temp_min}°C</span>
-                                    <span>/</span>
-                                    <span>{day.main.temp_max}</span>°C
-                                </div>
-                                <div>
-                                    Vent: <b>{day.wind.speed.toFixed(0)}</b> km/h
-                                </div>
-                                <div>
-                                    Humidité: <b>{day.main.humidity}</b> %
-                                </div>
-                            </div>
-                        </React.Fragment>
-                    ))
-                ) : (
-                    err ? (
-                        <span>{err}</span>
+                            </React.Fragment>
+                        ))
                     ) : (
-                        <span>Aucune donnée disponible</span>
-                    )
-                )}
+                        err ? (
+                            <span>{err}</span>
+                        ) : (
+                            <span>Aucune donnée disponible</span>
+                        )
+                    )}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
